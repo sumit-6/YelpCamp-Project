fc23b02540c79ccd1cbcd80724bb5dda4156fe9b
Sumit <vermasumit923@gmail.com>
2023-01-30 01:27:18 +0530

    Trying to deploy my app

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoDBStore = require('connect-mongodb-session')(session);
 
-//const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
+//const dbUrl = 'mongodb://localhost:27017/yelp-camp';
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -45,9 +45,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize());
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 const store = new MongoDBStore({
     uri : dbUrl,
-    secret: 'thisshouldbeabettersecret!',
+    secret: secret,
     touchAfter: 24 * 60 * 60
 });
 
@@ -57,7 +58,7 @@ store.on('error', function(error) {
 app.use(session({
     store,
     name: 'session',
-    secret: 'thisshouldbeabettersecret!',
+    secret: secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
