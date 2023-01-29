78dd94f9bb0d84b2fb1c6bba0ebeb875baef6774
Sumit <vermasumit923@gmail.com>
2023-01-30 02:20:42 +0530

    changed index.js

--- api/index.js
+++ api/index.js
@@ -39,11 +39,11 @@ const app = express();
 app.engine('ejs', ejsMate);
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(mongoSanitize());
 const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 const store = new MongoDBStore({
